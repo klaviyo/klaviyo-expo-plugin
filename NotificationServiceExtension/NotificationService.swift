5b3aa13f2a22b3c7a8ea300a0b448119cac89ec4
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-05-12 16:40:42 -0400

    Partway to creating NSE

---
+++ NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,24 @@
+import UserNotifications
+import KlaviyoSwiftExtension
+
+class NotificationService: UNNotificationServiceExtension {
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here if needed
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+} 
--- plugin/withKlaviyoIos.ts
+++ plugin/withKlaviyoIos.ts
@@ -1,4 +1,4 @@
-import { ConfigPlugin, withDangerousMod, withInfoPlist } from '@expo/config-plugins';
+import { ConfigPlugin, withDangerousMod, withInfoPlist, withXcodeProject } from '@expo/config-plugins';
 import { KlaviyoPluginIosConfig, KlaviyoPluginProps } from './types';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -23,9 +23,95 @@ const withRemoteNotificationsPermissions: ConfigPlugin<KlaviyoPluginProps> = (
   });
 };
 
+export const NSE_TARGET_NAME = "NotificationServiceExtension";
+export const NSE_EXT_FILES = [
+  "NotificationService.swift",
+  `${NSE_TARGET_NAME}.entitlements`,
+  `${NSE_TARGET_NAME}-Info.plist`
+];
+
+const withKlaviyoXcodeProject: ConfigPlugin<KlaviyoPluginProps> = (config) => {
+  return withXcodeProject(config, async (config) => {
+    const xcodeProject = config.modResults;
+    const projectName = xcodeProject.getFirstProject().firstProject.name;
+    const extGroup = xcodeProject.addPbxGroup(NSE_EXT_FILES, NSE_TARGET_NAME, NSE_TARGET_NAME);
+
+    // Add the new PBXGroup to the top level group. This makes the
+    // files / folder appear in the file explorer in Xcode.
+    const groups = xcodeProject.hash.project.objects["PBXGroup"];
+    Object.keys(groups).forEach(function(key) {
+      if (typeof groups[key] === "object" && groups[key].name === undefined && groups[key].path === undefined) {
+        xcodeProject.addToPbxGroup(extGroup.uuid, key);
+      }
+    });
+    // WORK AROUND for codeProject.addTarget BUG
+    // Xcode projects don't contain these if there is only one target
+    // An upstream fix should be made to the code referenced in this link:
+    //   - https://github.com/apache/cordova-node-xcode/blob/8b98cabc5978359db88dc9ff2d4c015cba40f150/lib/pbxProject.js#L860
+    const projObjects = xcodeProject.hash.project.objects;
+    projObjects['PBXTargetDependency'] = projObjects['PBXTargetDependency'] || {};
+    projObjects['PBXContainerItemProxy'] = projObjects['PBXTargetDependency'] || {};
+
+    // Add the NSE target
+    // This adds PBXTargetDependency and PBXContainerItemProxy for you
+    const nseTarget = xcodeProject.addTarget(NSE_TARGET_NAME, "app_extension", NSE_TARGET_NAME, `${config.ios?.bundleIdentifier}.${NSE_TARGET_NAME}`);
+
+    // Add build phases to the new target
+    // xcodeProject.addBuildPhase(
+    //   ["NotificationService.m"],
+    //   "PBXSourcesBuildPhase",
+    //   "Sources",
+    //   nseTarget.uuid
+    // );
+    // xcodeProject.addBuildPhase([], "PBXResourcesBuildPhase", "Resources", nseTarget.uuid);
+
+    // xcodeProject.addBuildPhase(
+    //   [],
+    //   "PBXFrameworksBuildPhase",
+    //   "Frameworks",
+    //   nseTarget.uuid
+    // );
+
+    return config;
+  });
+};
+
+const withKlaviyoNSE: ConfigPlugin<KlaviyoPluginProps> = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async config => {
+      const iosRoot = path.join(config.modRequest.projectRoot, "ios");
+      const nsePath = path.join(iosRoot, NSE_TARGET_NAME);
+      
+      // Create NSE directory if it doesn't exist
+      if (!FileManager.dirExists(nsePath)) {
+        fs.mkdirSync(nsePath, { recursive: true });
+      }
+
+      // Copy NSE files from the plugin root directory
+      const sourceDir = path.join(config.modRequest.projectRoot, "..", NSE_TARGET_NAME);
+      for (const file of NSE_EXT_FILES) {
+        try {
+          await FileManager.copyFile(
+            path.join(sourceDir, file),
+            path.join(nsePath, file)
+          );
+        } catch (error) {
+          console.error(`Failed to copy ${file}:`, error);
+          throw error;
+        }
+      }
+
+      return config;
+    },
+  ]);
+};
+
 const withKlaviyoIos: ConfigPlugin<KlaviyoPluginProps> = (config, props) => {
   config = withRemoteNotificationsPermissions(config, props);
-  config = withKlaviyoPodfile(config, props)
+  config = withKlaviyoPodfile(config, props);
+  config = withKlaviyoNSE(config, props);
+  config = withKlaviyoXcodeProject(config, props);
   return config;
 };
 
