d75dbd9e1a370c2d8adbc321cab3f9078b9edf54
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-09-12 14:29:00 -0400

    Remove unnecessary funcs

--- ios/ExpoKlaviyo/KlaviyoAppDelegate.swift
+++ ios/ExpoKlaviyo/KlaviyoAppDelegate.swift
@@ -3,19 +3,7 @@ import KlaviyoSwift
 
 public final class KlaviyoAppDelegate: ExpoAppDelegateSubscriber, UNUserNotificationCenterDelegate {
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Initialize Klaviyo SDK
-        // Note: This will be called by KlaviyoSdkAppDelegateHandler for non-swizzling support
         return true
-    }
-    
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // Handle device token registration
-        // This will be called by KlaviyoSdkAppDelegateHandler
-    }
-    
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        // Handle registration failure
-        // This will be called by KlaviyoSdkAppDelegateHandler
     }
     
     public func userNotificationCenter(
@@ -33,7 +21,6 @@ public final class KlaviyoAppDelegate: ExpoAppDelegateSubscriber, UNUserNotifica
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
     ) {
-        // Allow notifications to be presented when app is in foreground
         completionHandler([.list, .banner, .badge, .sound])
     }
 }
