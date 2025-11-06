d1786436b890312f393883bb056dd33e007a93b6
Dan Peluso <daniel.peluso@klaviyo.com>
2025-11-06 15:26:13 -0500

    EAS Expo 54  Support (#81)
    * referencing remote branch
    
    * eas builds working
    
    * changing expo plugin path to local

---
+++ example/app.config.js
@@ -0,0 +1,82 @@
+module.exports = () => {
+  return {
+    name: 'klaviyo plugin example',
+    slug: 'klaviyo-plugin-example',
+    version: '0.1.2',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'expoexample',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: false,
+    ios: {
+      bundleIdentifier: 'com.klaviyo.expoexample',
+      infoPlist: {
+        UIBackgroundModes: ['remote-notification'],
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'com.klaviyo.expoexample',
+      jsEngine: 'hermes',
+      // For EAS builds with secret uploaded
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || './google-services.json',
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'klaviyo-expo-plugin',
+        {
+          android: {
+            logLevel: 1,
+            openTracking: true,
+            notificationIconFilePath: './assets/images/ic_notification.png',
+            notificationColor: '#FF0000',
+          },
+          ios: {
+            badgeAutoclearing: true,
+            codeSigningStyle: 'Automatic',
+            projectVersion: '3',
+            marketingVersion: '0.1.2',
+            devTeam: 'XXXXXXXXXX',
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    assetBundlePatterns: ['**/*'],
+    extra: {
+      router: {},
+      eas: {
+        projectId: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
+        build: {
+          experimental: {
+            ios: {
+              appExtensions: [
+                {
+                  targetName: 'KlaviyoNotificationServiceExtension',
+                  bundleIdentifier: 'com.klaviyo.expoexample.KlaviyoNotificationServiceExtension',
+                  entitlements: {
+                    'com.apple.security.application-groups': [
+                      'group.com.klaviyo.expoexample.KlaviyoNotificationServiceExtension.shared',
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+    owner: 'klaviyo',
+  };
+};
